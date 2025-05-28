62e489086c39f87c9a39ed5486577e05d592e67b
Janyah-Armstrongs <Janyaharmstrong091406@gmail.com>
2025-05-28 12:48:45 -0400

    Updated Comments

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"   # Defines the IP address range
 }
 
 resource "aws_subnet" "public" {
@@ -10,10 +10,10 @@ resource "aws_subnet" "public" {
 resource "aws_instance" "web" {
   ami          = var.ami
   instance_type = var.instance_type
-  subnet_id    = aws_subnet.public.id
+  subnet_id    = aws_subnet.public.id    # Places the instance insode public subnet
 
 
   tags = {
-    Name = var.instance_name
+    Name = var.instance_name  # Tags instance with a name
   }
 }
