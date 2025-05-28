3fc885ab7ee501132bc72fca9d789cc4d1793389
Janyah-Armstrongs <Janyaharmstrong091406@gmail.com>
2025-05-28 13:00:03 -0400

    Updated comments

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "instance_id" {
-  value = aws_instance.web.id
+  value = aws_instance.web.id # Displays the ID of EC2 Instacnes after 'terraform apply' 
 }
 
 output "subnet_id" {
-  value = aws_subnet.public.id
+  value = aws_subnet.public.id  # Displays the ID of subnet afte 'terraform apply'
 }
